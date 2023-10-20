d27a6b732f28145b4c59113674fbca8a7ce46baf
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-20 11:55:16 +0500

    kotlin extensions added

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.bottom_nav
+import kotlinx.android.synthetic.main.activity_main.frame_content
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bottom_nav
+        frame_content
     }
 }
