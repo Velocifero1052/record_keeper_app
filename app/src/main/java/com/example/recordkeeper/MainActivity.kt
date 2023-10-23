fdd96b67e734dd36557d98ca7a7a87f6b5069a50
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-23 15:46:47 +0500

    buttom navigation with removal of deprecation completed

--- app/src/main/java/com/example/recordkeeper/MainActivity.kt
+++ app/src/main/java/com/example/recordkeeper/MainActivity.kt
@@ -2,20 +2,51 @@ package com.example.recordkeeper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.commit
 import com.example.recordkeeper.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationBarView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    NavigationBarView.OnItemSelectedListener {
 
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
+        setContentView(binding.root)
+        binding.bottomNav.setOnItemSelectedListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        Toast.makeText(this, "I have made the activity", Toast.LENGTH_LONG).show()
+    }
+
+    private fun onRunningClicked() {
         supportFragmentManager.commit {
-            add(R.id.frame_content, RunningFragment())
+            replace(R.id.frame_content, RunningFragment())
         }
     }
+
+    private fun onCyclingClicked() {
+        supportFragmentManager.commit {
+            replace(R.id.frame_content, CyclingFragment())
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        return if(item.itemId == R.id.nav_running) {
+            onRunningClicked()
+            true
+        } else if(item.itemId == R.id.nav_cycling) {
+            onCyclingClicked()
+            true
+        } else {
+            false
+        }
+    }
+
 }
