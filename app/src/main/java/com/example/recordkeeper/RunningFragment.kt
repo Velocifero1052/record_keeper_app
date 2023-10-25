6b5ee8a5b6f9b3eba60458de93356a7e01d19afd
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-25 14:30:34 +0500

    running activity completed

--- app/src/main/java/com/example/recordkeeper/RunningFragment.kt
+++ app/src/main/java/com/example/recordkeeper/RunningFragment.kt
@@ -1,18 +1,41 @@
 package com.example.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recordkeeper.databinding.FragmentRunningBinding
 
 class RunningFragment : Fragment() {
+
+    private lateinit var binding: FragmentRunningBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-     ): View? {
-        return inflater.inflate(R.layout.fragment_running, container, false)
+     ): View {
+        binding = FragmentRunningBinding.inflate(inflater, container, false)
+        return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.container3km.setOnClickListener{ launchRunningRecordScreen() }
+        binding.container5km.setOnClickListener{ launchRunningRecordScreen() }
+        binding.containerHalfMarathon.setOnClickListener{ launchRunningRecordScreen() }
+        binding.containerFullMarathon.setOnClickListener{ launchRunningRecordScreen() }
+    }
+
+    private fun launchRunningRecordScreen() {
+        val intent = Intent(context, EditRunningRecordActivity::class.java)
+        startActivity(intent)
+    }
+
 }
