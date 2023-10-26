0a71ae11b90fd107a3079aae224be2046c862ba4
Velocifero1052 <commelevent1052@yandex.ru>
2023-10-26 15:28:46 +0500

    new activities, edit running and edit text

--- app/src/main/java/com/example/recordkeeper/CyclingFragment.kt
+++ app/src/main/java/com/example/recordkeeper/CyclingFragment.kt
@@ -1,18 +1,46 @@
 package com.example.recordkeeper
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.recordkeeper.databinding.FragmentCyclingBinding
 
 class CyclingFragment: Fragment() {
+
+    private lateinit var binding: FragmentCyclingBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_cycling, container, false)
+    ): View {
+        binding = FragmentCyclingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.longestRideContainer.setOnClickListener {
+            launchRunningRecordScreen(binding.longestRideTextView.text.toString())
+        }
+        binding.biggestClimbContainer.setOnClickListener {
+            launchRunningRecordScreen(binding.biggestClimbTextView.text.toString())
+        }
+        binding.bestAverageSpeedContainer.setOnClickListener {
+            launchRunningRecordScreen(binding.bestAverageSpeedTextView.text.toString())
+        }
+    }
+
+    private fun launchRunningRecordScreen(distance: String) {
+        val intent = Intent(context, EditCyclingRecordActivity::class.java)
+        intent.putExtra("distance", distance)
+        startActivity(intent)
     }
 }
